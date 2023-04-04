81a5f6bfb780659aa4668b9f594a22df54e549d5
Jakob Handke <jakobhandke@icloud.com>
2023-04-04 17:42:34 +0200

    fix code generation

--- src/main/clojureblocks/generator/clojure.cljs
+++ src/main/clojureblocks/generator/clojure.cljs
@@ -1,9 +1,10 @@
 (ns clojureblocks.generator.clojure
   (:require ["blockly" :as blockly]
             [goog.object :as g]
-            [clojureblocks.generator.literals]))
+            [clojureblocks.generator.literals]
+            [clojureblocks.generator.math]))
 
 (def clojure-generator (blockly/Generator. "clojure"))
 
-(set! (.. clojure-generator "number_block") clojureblocks.generator.literals/generate-number-literal)
-(set! (.. clojure-generator "add_block") clojureblocks.generator.math/generate-adda-function)
+(set! (.. clojure-generator -number-block) clojureblocks.generator.literals/generate-number-literal)
+(set! (.. clojure-generator  -add_block) clojureblocks.generator.math/generate-add-function)
--- src/main/clojureblocks/generator/math.cljs
+++ src/main/clojureblocks/generator/math.cljs
@@ -1,4 +1,4 @@
 (ns clojureblocks.generator.math)
 
 (defn generate-add-function [block]
-  )
+  "to be implemented")
