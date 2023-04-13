a7e5d6930a9f330dba789c235ac524d3e083262f
Jakob Handke <jakobhandke@icloud.com>
2023-04-13 16:46:19 +0200

    add map block preset

--- src/main/clojureblocks/toolbox.cljs
+++ src/main/clojureblocks/toolbox.cljs
@@ -1,12 +1,27 @@
 (ns clojureblocks.toolbox
   (:require [clojureblocks.blocks.all]))
 
-(defn generate-toolbox []
-  {:kind "flyoutToolbox"
-   :contents (map
-               (fn [block]
-                 (let [name (get block :type)]
-                   {:kind "block"
-                    :type name}))
-               (clojureblocks.blocks.all/blocks))})
+(def map-block-preset
+  {:kind "block"
+   :type "hof_map_block"
+   :inputs {:hof-map-function
+            {:block {:type "symbol_block"
+                     :fields {:symbol_name "inc"}}}
+            :hof-map-collection 
+            {:block {:type "list_block"
+                     :inputs {:list-items
+                              {:block {:type "symbol_block"
+                                       :fields {:symbol_name "range"}
+                                       :next
+                                       {:block {:type "number_block"
+                                                :fields {:number 10}}}}}}}}}})
 
+(defn generate-toolbox []
+  (let [all-blocks (map
+                    (fn [block]
+                      (let [name (get block :type)]
+                        {:kind "block"
+                         :type name}))
+                    (clojureblocks.blocks.all/blocks))]
+    {:kind "flyoutToolbox"
+     :contents (conj (vec all-blocks) map-block-preset)}))
