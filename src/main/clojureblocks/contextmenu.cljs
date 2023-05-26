e47403b183df6b5ef2744ddbc4eb7d76f6ec40f9
Jakob Handke <jakobhandke@icloud.com>
2023-05-26 21:46:40 +0200

    make context menu dispatch more readable

--- src/main/clojureblocks/contextmenu.cljs
+++ src/main/clojureblocks/contextmenu.cljs
@@ -3,28 +3,29 @@
             [clojureblocks.hof-inspection :as inspection]
             [clojureblocks.modal-view :as modal-view]))
 
-(defn block-inspection [scope]
-  (cond ;; multimethods!!!
-    (= "hof_map_block" (.. scope -block -type))
-    (modal-view/display-lines (inspection/map-inspection (. scope -block)))
-    
-    (= "hof_reduce_block" (.. scope -block -type))
-    (modal-view/display-lines (inspection/reduce-inspection (. scope -block)))
-    
-    (= "hof_filter_block" (.. scope -block -type))
-    (modal-view/display-lines (inspection/filter-inspection (. scope -block)))))
+(defmulti context-menu-action (fn [scope] (.. scope -block -type)))
+
+(defmethod context-menu-action "hof_map_block"
+  [scope]
+  (modal-view/display-lines (inspection/map-inspection (. scope -block))))
+
+(defmethod context-menu-action "hof_reduce_block"
+  [scope]
+  (modal-view/display-lines (inspection/reduce-inspection (. scope -block))))
+
+(defmethod context-menu-action "hof_filter_block"
+  [scope]
+  (modal-view/display-lines (inspection/filter-inspection (. scope -block))))
 
 (defn precondition [scope]
-  (cond
-    (or (= "hof_map_block" (.. scope -block -type)) (= "hof_reduce_block" (.. scope -block -type)) (= "hof_filter_block" (.. scope -block -type)))
+  (if (contains? #{"hof_map_block" "hof_reduce_block" "hof_filter_block"} (.. scope -block -type))
     "enabled"
-    :else
     "disabled"))
 
 (def hof-inspection-contextmenu-item
   {:id "hof-inspection"
    :callback (fn [scope]
-               (block-inspection scope))
+               (context-menu-action scope))
    :scopeType (. (. ContextMenuRegistry -ScopeType) -BLOCK)
    :displayText (fn [] "Inspect")
    :preconditionFn precondition
