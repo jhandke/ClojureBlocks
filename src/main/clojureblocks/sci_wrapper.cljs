837d00b5409780a683f554a4f52426fce2c55913
Jakob Handke <jakobhandke@icloud.com>
2023-06-05 15:57:51 +0200

    add console printing

--- src/main/clojureblocks/sci_wrapper.cljs
+++ src/main/clojureblocks/sci_wrapper.cljs
@@ -1,39 +1,43 @@
 (ns clojureblocks.sci-wrapper
   (:require [sci.core :as sci]))
 
+(enable-console-print!)
+(sci/alter-var-root sci/print-fn (constantly *print-fn*))
+(sci/alter-var-root sci/print-err-fn (constantly *print-err-fn*))
+
 (def sci-options {})
 (def sci-context (atom (sci/init sci-options)))
+(def clojureblocks-print-length (atom 12))
 
-(defn emulate-print 
+(defn emulate-print
   "Surrounds `expression` with (pr-str ...) for print-like behaivour."
-  [expression] 
+  [expression]
   (str "(pr-str " expression ")"))
 
-(defn evaluate 
+(defn evaluate
   "Evaluates a single `expression`."
-  [expression] 
-  (sci/binding [sci/print-length 12]
+  [expression]
+  (sci/binding [sci/print-length @clojureblocks-print-length]
     {:expression expression
      :result (sci/eval-string* @sci-context (emulate-print expression))}))
 
-(defn evaluate-with-error 
+(defn evaluate-with-error
   "Evaluates a single `expression` and return error."
   [expression]
-  (sci/binding [sci/print-length 12]
-    (try 
+  (sci/binding [sci/print-length @clojureblocks-print-length]
+    (try
       (hash-map :expression expression
                 :result (sci/eval-string* @sci-context (emulate-print expression)))
-      (catch js/Error e 
+      (catch js/Error e
         (hash-map :expression expression
-                  :error (. e -message)))
-      )))
+                  :error (. e -message))))))
 
 (defn evaluate-inspection
   "Evaluates a single `expression` for internal use."
   [expression]
   (sci/eval-string* @sci-context expression))
 
-(defn reset-context 
+(defn reset-context
   "Resets the sci-context (like restarting a repl)."
   []
   (swap! sci-context (fn [_] (sci/init sci-options))))
