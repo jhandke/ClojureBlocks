3c7d8bee028b5c75d36ee0de27c3eb600a4d3954
Jakob Handke <jakobhandke@icloud.com>
2023-07-19 14:59:14 +0200

    add doc comments

--- src/main/clojureblocks/generator/sets.cljs
+++ src/main/clojureblocks/generator/sets.cljs
@@ -2,18 +2,21 @@
   (:require [clojureblocks.generator.clojure :as generator]))
 
 (defn generate-union-block
+  "Generates the code for a union block: `(union set1 set2)`"
   [block]
   (let [set1 (generator/generate-statement-code block "set1")
         set2 (generator/generate-statement-code block "set2")]
     (generator/expression "clojure.set/union" set1 set2)))
 
 (defn generate-intersection-block
+  "Generates the code for an intersection block: `(intersection set1 set2)`"
   [block]
   (let [set1 (generator/generate-statement-code block "set1")
         set2 (generator/generate-statement-code block "set2")]
     (generator/expression "clojure.set/intersection" set1 set2)))
 
 (defn generate-difference-block
+  "Generates the code for a difference block: `(difference set1 set2)`"
   [block]
   (let [set1 (generator/generate-statement-code block "set1")
         set2 (generator/generate-statement-code block "set2")]
